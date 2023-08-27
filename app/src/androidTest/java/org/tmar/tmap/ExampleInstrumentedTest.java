4e959206b5a6fe491356a6ab2b4e1449756febc6
nallemauste <nallemauste@gmail.com>
2023-08-27 21:23:29 +0300

    Fix package name.

--- app/src/androidTest/java/org/tmar/tmap/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/tmar/tmap/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("org.mur.kapsitiles", appContext.getPackageName());
+        assertEquals("org.tmar.tmap", appContext.getPackageName());
     }
 }
